be65ad288fffa1a52e99b80b05be36768ce768cc
RajiaRani <Rajiarani.syal@gmail.com>
2025-11-18 16:27:56 -0600

    added new code file

--- 1. Python Fundamentals(Part 1)/02. Operaters.py
+++ 1. Python Fundamentals(Part 1)/02. Operaters.py
@@ -2,17 +2,21 @@
 
 num1 = 20
 num2 = 30
-sum = num1 + num2
-print("Sum", sum)
+print("Sum", num1 + num2)
 print("Subtraction", num1- num2)
 print("Multiplication", num1*num2)
-print("Modulu", num1%num2)
+print("Modulu print the reminder  ", num1%num2)
 print("Divide", num1/num2)
 
 
 # Relational/ Comparison
 
-
+a = 10
+b = 14
+print(a>b)
+print(a>b)
+print(a == b)
+print(a!=b)
 # Assignment
 
 

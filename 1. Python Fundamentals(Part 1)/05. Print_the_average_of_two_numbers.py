74333028d38054e005ac3b324b12f6b16e245567
RajiaRani <Rajiarani.syal@gmail.com>
2025-11-18 17:45:23 -0600

    added new code file - based on user input

---
+++ 1. Python Fundamentals(Part 1)/05. Print_the_average_of_two_numbers.py
@@ -0,0 +1,11 @@
+num1 = 2
+num2 = 2
+average = (num1+num2)/ 2
+print("Average: ", average)
+
+
+
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+avg = (a+b)/2
+print("Average: ", avg)

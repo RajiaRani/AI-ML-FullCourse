716ad22bb8cac6b10d49e793e047e528bb918c47
RajiaRani <Rajiarani.syal@gmail.com>
2025-11-18 17:41:48 -0600

    added new code file - based on user input

--- 1. Python Fundamentals(Part 1)/04. User_Input.py
+++ 1. Python Fundamentals(Part 1)/04. User_Input.py
@@ -1,5 +1,14 @@
+# Program 1
 a = input("Enter the value of a")
-print(a)
+# print(a)
 
+# Program 2
 name = input("Enter your name")
-print("Welcome ", name)
+# print("Welcome ", name)
+
+
+# Program 3
+num1 = int(input("Enter the number first"))
+num2 = int(input('Enter the number second'))
+sum = num1 + num2
+print("Overall Sum = ", sum) 

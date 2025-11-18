de6f26e4f3974d5152b9134524d8752b11d4554b
RajiaRani <Rajiarani.syal@gmail.com>
2025-11-18 15:42:40 -0600

    added new code file

--- 1. Python Fundamentals(Part 1)/02. Operaters.py
+++ 1. Python Fundamentals(Part 1)/02. Operaters.py
@@ -1,9 +1,16 @@
 #  Arithmetic = +, -, %, *, /, 
 
+# Sum Operator
 num1 = 20
 num2 = 30
 sum = num1 + num2
 print("Sum", sum)
+
+# Minus Operator
+num3 = 45
+num4 = 23
+minus = num3 - num4
+print("Minus",  minus)
 
 # Relational/ Comparison
 

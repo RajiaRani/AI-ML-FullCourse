32827219070b5646a2ea9503869676c9b02fb88f
RajiaRani <Rajiarani.syal@gmail.com>
2025-11-23 20:07:46 -0600

    added new folder

--- 02. Python Fundamentals(Part 2)/11.Functions.py
+++ 02. Python Fundamentals(Part 2)/11.Functions.py
@@ -12,3 +12,33 @@ sum(2,2)
 sub(1,2)
 multi(3,2)
 
+
+
+def Sum(a,b):
+    s = a+b
+    return s
+
+ans = Sum(4,5)
+print(ans)
+
+
+# Example
+def average(a, b, c):
+    avg = (a+b+c)/3
+    return avg
+ans = average(3,3,3)
+print(ans)
+
+# OR 
+def average(a, b, c):
+    sum = a+b+c
+    return sum/3
+ans = average(3,3,3)
+print(ans)
+
+# Defining the dafault parameters
+def find_sum(a, b=1):
+    s = a+b
+    return s
+
+print(find_sum(4))
---
+++ 02. Python Fundamentals(Part 2)/12.Lambda.py
@@ -0,0 +1,8 @@
+# 1
+sum = lambda a, b: a+b
+print(sum(2,23))
+
+# 2
+avg = lambda a, b: (a+b)/2
+print(avg(4,4))
+

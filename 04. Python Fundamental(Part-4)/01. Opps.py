96844d6d08fd62281f6a24fd0c392aeb39c5b164
RajiaRani <Rajiarani.syal@gmail.com>
2025-11-30 20:09:57 -0600

    added new part

--- 04. Python Fundamental(Part-4)/01. Opps.py
+++ 04. Python Fundamental(Part-4)/01. Opps.py
@@ -1,4 +1,4 @@
 # Opps - object oriendted programming - mapping real word objects
 
-def sum(a,b):
-    return a+b
+# Class - blueprint of an object -- class is NOT a variable
+# Object - instance of class -- means object is the actual thing which takes place in memeory or else occupied the memory

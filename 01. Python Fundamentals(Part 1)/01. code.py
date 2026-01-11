3f29baf00d41a3a93776c92e0b69dc9b7805ab51
RajiaRani <Rajiarani.syal@gmail.com>
2026-01-10 22:56:35 -0600

    added

--- 01. Python Fundamentals(Part 1)/01. code.py
+++ 01. Python Fundamentals(Part 1)/01. code.py
@@ -4,3 +4,24 @@ name="Rajia Syal"
 roll_number = 114356
 
 print("Hello", name)
+
+print('AI','ML')
+print(5 * "A")
+print(5 * "A")
+
+a = 10
+b = 20 
+print(a,b)
+print(a+b)
+
+a1 = 10
+b1 = "20"
+print(str(a1) + b1)
+print(a1 + int(b1))
+
+x = 2
+y  = 3
+print(x ** y)
+x1 = "2"
+y2 = 3
+print(x1 * y2)
--- 04. Python Fundamental(Part-4)/11. Mini_Chat_System.py
+++ 04. Python Fundamental(Part-4)/11. Mini_Chat_System.py
@@ -11,3 +11,19 @@ class Message:
     def __str__(self):
         return f"({self.id}) {self.sender.username}:{self.content}"
     
+
+# -------------------------------# User Class# -------------------------------
+class User:
+def __init__(self, username):
+    self.username = username
+    self.chatroom = None
+    def join_chatroom(self, chatroom):
+        if self.chatroom:
+            print(f"{self.username} is already in a chatroom.")
+            else:
+            chatroom.add_user(self)
+            self.chatroom = chatroom
+            print(f"{self.username} joined {chatroom.name}")
+            def leave_chatroom(self):
+                if not self.chatroom:
+            

e7e61ab2162fb89311e4de794e84e52bbee2222e
RajiaRani <Rajiarani.syal@gmail.com>
2026-03-23 23:30:29 -0500

    added updated

--- 02. Python Fundamentals(Part 2)/14.All_Loops_Questions.py
+++ 02. Python Fundamentals(Part 2)/14.All_Loops_Questions.py
@@ -1,12 +1,16 @@
-# Print the multipication table for any number
-n = int(input("Enter any number: "))
-i = 1
-while(i<=10):
-    print(i*n)
-    i+=1
-
-# reverse the table
-i = 10
-while(i>=1):
-    print(i*n)
-    i-=1
+{
+ "cells": [],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "name": "python",
+   "version": "3.12.12"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
